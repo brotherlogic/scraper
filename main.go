cb31340e3d7673dc415c88a7f7ceb32d06436423
Simon Tucker <brotherlogic@gmail.com>
2024-01-01 05:08:45 -0800

    Got scraper working

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
+	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/chromedp"
+	"github.com/go-rod/stealth"
 	"google.golang.org/grpc"
+
+	cu "github.com/Davincible/chromedp-undetected"
 
 	pb "github.com/brotherlogic/scraper/proto"
 )
@@ -20,8 +26,42 @@ var (
 type Server struct{}
 
 func (s *Server) Scrape(ctx context.Context, req *pb.ScrapeRequest) (*pb.ScrapeResponse, error) {
+	// New creates a new context for use with chromedp. With this context
+	// you can use chromedp as you normally would.
+	ctx, cancel, err := cu.New(cu.NewConfig(
+		// Remove this if you want to see a browser window.
+		cu.WithHeadless(),
 
-	return nil, nil
+		// If the webelement is not found within 10 seconds, timeout.
+		cu.WithTimeout(10*time.Second),
+	))
+	if err != nil {
+		return nil, err
+	}
+	defer cancel()
+
+	html := ""
+
+	err = chromedp.Run(ctx,
+		chromedp.Evaluate(stealth.JS, nil),
+		chromedp.Navigate(req.GetUrl()),
+		chromedp.Sleep(2000*time.Millisecond),
+		// extract the raw HTML from the page
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			// select the root node on the page
+			rootNode, err := dom.GetDocument().Do(ctx)
+			if err != nil {
+				return err
+			}
+			html, err = dom.GetOuterHTML().WithNodeID(rootNode.NodeID).Do(ctx)
+			return err
+		}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ScrapeResponse{Body: html}, nil
 }
 
 func main() {
